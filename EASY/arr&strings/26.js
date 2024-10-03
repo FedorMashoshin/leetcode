8bc9ee45455b90dcdab7e59dd913c726709fbd6a
FedorMAshoshin <fmashoshin@gmail.com>
2024-10-02 19:32:32 -0700

    26 edge case

--- EASY/arr&strings/26.js
+++ EASY/arr&strings/26.js
@@ -1,6 +1,7 @@
 // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
 const removeDuplicates = function (nums) {
+    if (nums.length === 0) return 0;
     let i = 0;
     for (let j = 1; j < nums.length; j++) {
         if (nums[i] !== nums[j]) {
